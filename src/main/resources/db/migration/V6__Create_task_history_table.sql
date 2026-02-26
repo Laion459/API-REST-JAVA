603cc18e209f0d0514f1d1bf4dacf1c3b447193c
Leonardo Borges <laion459@gmail.com>
2026-02-26 11:48:17 -0300

    refactor: translate configuration files to English
    - Translate pom.xml comments and description
    - Translate application.yml comments
    - Translate logback-spring.xml comments
    - Translate SQL migration comments

--- src/main/resources/db/migration/V6__Create_task_history_table.sql
+++ src/main/resources/db/migration/V6__Create_task_history_table.sql
@@ -1,4 +1,4 @@
--- Tabela para histórico de mudanças de tasks (auditoria de dados)
+-- Table for task change history (data auditing)
 CREATE TABLE IF NOT EXISTS task_history (
     id BIGSERIAL PRIMARY KEY,
     task_id BIGINT NOT NULL,
@@ -14,9 +14,9 @@ CREATE INDEX IF NOT EXISTS idx_task_history_task_id ON task_history(task_id);
 CREATE INDEX IF NOT EXISTS idx_task_history_changed_at ON task_history(changed_at);
 CREATE INDEX IF NOT EXISTS idx_task_history_field ON task_history(field_name);
 
--- Comentários para documentação
-COMMENT ON TABLE task_history IS 'Histórico de mudanças de campos de tasks para auditoria';
-COMMENT ON COLUMN task_history.field_name IS 'Nome do campo que foi alterado';
-COMMENT ON COLUMN task_history.old_value IS 'Valor anterior do campo';
-COMMENT ON COLUMN task_history.new_value IS 'Novo valor do campo';
-COMMENT ON COLUMN task_history.changed_by IS 'Username que fez a alteração';
+-- Comments for documentation
+COMMENT ON TABLE task_history IS 'Task field change history for auditing';
+COMMENT ON COLUMN task_history.field_name IS 'Name of the field that was changed';
+COMMENT ON COLUMN task_history.old_value IS 'Previous field value';
+COMMENT ON COLUMN task_history.new_value IS 'New field value';
+COMMENT ON COLUMN task_history.changed_by IS 'Username who made the change';
